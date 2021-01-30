fca22ee09030df1883bb11e69219cc0522d66f31
israelermel <israelermel@gmail.com>
2021-01-30 12:11:56 -0300

    melhorias na lista de melhories repositórios kotlin

--- features/feature-top-ranked/src/main/java/br/com/israelermel/feature_top_ranked/adapters/repos/ReposLoadStateAdapter.kt
+++ features/feature-top-ranked/src/main/java/br/com/israelermel/feature_top_ranked/adapters/repos/ReposLoadStateAdapter.kt
@@ -1,17 +1,12 @@
 package br.com.israelermel.feature_top_ranked.adapters.repos
 
-import android.util.Log
 import android.view.ViewGroup
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 
 class ReposLoadStateAdapter(private val retry: () -> Unit) : LoadStateAdapter<ReposLoadStateViewHolder>() {
     override fun onBindViewHolder(holder: ReposLoadStateViewHolder, loadState: LoadState) {
-        try {
-            holder.bind(loadState)
-        } catch (e: Exception) {
-            Log.e("israel-error", e.message.orEmpty())
-        }
+        holder.bind(loadState)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): ReposLoadStateViewHolder {
